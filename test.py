ccbf279aaa474fe9e11fb22f7a1c5c53e0cbd93c
crawfordxx <ksgt950817@gmail.com>
2023-10-01 23:10:12 +0800

    initial commit

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
 ##test
+import openai
+from pptx import Presentation
+
+openai.api_key = "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"
+
+prs = Presentation()
+
+# Generate text using OpenAI API
+response = openai.Completion.create(engine="davinci", prompt="Title: Benefits of Artificial Intelligence", max_tokens=150)
+generated_text = response['choices'][0]['text'].strip()
+
+# Split generated text into title and content
+title, content = generated_text.split('\n', 1)
+
+# Add a slide with a title and content
+slide = prs.slides.add_slide(prs.slide_layouts[1])
+title_shape = slide.shapes.title
+title_shape.text = title
+content_box = slide.placeholders[1]
+content_box.text = content
+
+# Save the presentation
+prs.save('presentation.pptx')
